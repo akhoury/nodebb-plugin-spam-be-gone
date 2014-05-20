971f0af6cdeca34547ea472fa147330c9f4fad2e
Aziz Khoury <bentael@gmail.com>
2014-05-20 12:51:20 -0400

    injecting recaptcha_ajax.js instead of including it within the plugin.scripts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,23 +6,35 @@ $(function() {
 			&& plugin['spam-be-gone']
 			&& plugin['spam-be-gone'].recaptchaArgs
 			&& $('#' + plugin['spam-be-gone'].recaptchaArgs.targetId).length
-		) {
-			var args = plugin['spam-be-gone'].recaptchaArgs;
-			Recaptcha.create(
-				args.publicKey,
-				args.targetId,
-				{
-					theme: args.options.theme,
-					lang: args.options.lang,
-					tabIndex: args.options.tabindex,
-					callback: function() {
-						var error = utils.param('error');
-						if (error) {
-							app.alertError(error);
+			) {
+
+			var createCaptcha = function() {
+				var args = plugin['spam-be-gone'].recaptchaArgs;
+
+				if (window.Recaptcha) {
+					Recaptcha.create(
+						args.publicKey,
+						args.targetId,
+						{
+							theme: args.options.theme,
+							lang: args.options.lang,
+							tabIndex: args.options.tabindex,
+							callback: function() {
+								var error = utils.param('error');
+								if (error) {
+									app.alertError(error);
+								}
+							}
 						}
-					}
+					);
 				}
-			);
+			};
+
+			if ($('script[scr$="recaptcha_ajax.js"]').length) {
+				createCaptcha();
+			} else {
+				utils.injectScript('//www.google.com/recaptcha/api/js/recaptcha_ajax.js', {onload: createCaptcha});
+			}
 		}
 	});
-})
+});
