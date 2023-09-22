816c9a1df088aa07e5e9f133ca3bd6d51aadb9c2
LingkongSky <lingkongsky@gmail.com>
2023-09-22 23:06:58 +0800

    add some translate files

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 
 const util = require('util');
 const Honeypot = require('project-honeypot');
-const simpleRecaptcha = require('simple-recaptcha-new');
+//const simpleRecaptcha = require('simple-recaptcha-new');
 const hCaptcha = require('hcaptcha');
 const stopforumspam = require('stopforumspam');
 
